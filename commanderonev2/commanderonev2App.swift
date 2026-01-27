a095d5c20801937b9adda60a4358c6e39923f7c7
Joao Ferreira <ferreyrajoao@gmail.com>
2026-01-27 10:11:52 +0000

    Initial Commit

---
+++ commanderonev2/commanderonev2App.swift
@@ -0,0 +1,17 @@
+//
+//  commanderonev2App.swift
+//  commanderonev2
+//
+//  Created by Joao Ferreira on 27/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct commanderonev2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
